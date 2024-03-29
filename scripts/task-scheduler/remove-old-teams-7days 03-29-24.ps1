cbafd86ef9d01c5b20ffab63eaf4d45a1ab4e816
Toby vanRoojen <toby@vanroojen.com>
2024-03-29 13:44:15 -0700

    h

--- scripts/task-scheduler/remove-old-teams-7days 03-29-24.ps1
+++ scripts/task-scheduler/remove-old-teams-7days 03-29-24.ps1
@@ -1,3 +1,10 @@
+
+<# the "MM-DD-YY" at the end of the filename governs if a new version is downloaded and deployed. If saving changes you want implemented, ensure the date is changed to the current date.
+
+This script is grabbed by the silent-launcher.ps1 script set to run at each user login. that launcherwon't download a changed version of this file unless the date is changed. Only the latest date will be used. 
+
+#>
+
 # Setup logging infrastructure
 $logDir = "C:\install\logs"
 $username = $env:USERNAME
