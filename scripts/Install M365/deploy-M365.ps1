f1501742b34f7d3f03ba25c12b6920afec696f23
Toby vanRoojen <toby@vanroojen.com>
2024-04-01 11:16:07 -0700

    Create deploy-M365.ps1

---
+++ scripts/Install M365/deploy-M365.ps1
@@ -0,0 +1,25 @@
+$prepPath = "c:\install\avd-prep\"
+
+# Major Section: Installing Microsoft 365
+# -----------------------------------------------------
+$odtFolder = Join-Path -Path $prepPath -ChildPath "ODT"
+if (-not (Test-Path -Path $odtFolder)) {
+    New-Item -ItemType Directory -Path $odtFolder | Out-Null
+}
+
+# Download the ODT setup executable
+$odtExePath = Join-Path -Path $odtFolder -ChildPath "officedeploymenttool_17328-20162.exe"
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_17328-20162.exe" -OutFile $odtExePath
+
+# Extract the ODT contents
+Start-Process -FilePath $odtExePath -ArgumentList "/quiet /extract:`"$odtFolder`"" -NoNewWindow -Wait
+
+# Assuming the ODT contents, including 'setup.exe', are extracted directly into $odtFolder
+$setupPath = Join-Path -Path $odtFolder -ChildPath "setup.exe"
+
+# Download the XML configuration file
+$xmlFilePath = Join-Path -Path $odtFolder -ChildPath "OGE_Configuration.xml"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tvanroo/oger/main/scripts/install/OGE_Configuration.xml" -OutFile $xmlFilePath
+
+# Use the extracted 'setup.exe' for the Office installation/configuration
+Start-Process -FilePath $setupPath -ArgumentList "/configure `"$xmlFilePath`"" -NoNewWindow -Wait
