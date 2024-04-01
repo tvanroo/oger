3902b1b9fbab26dda6f2c16323e60c9a5f6c4e39
Toby vanRoojen | WA (PST) <toby.vanroojen@fedscale.com>
2024-04-01 13:08:54 -0700

    Update avd-prep-scripts.ps1

--- scripts/avd-prep-scripts.ps1
+++ scripts/avd-prep-scripts.ps1
@@ -20,29 +20,10 @@
         New-Item -ItemType Directory -Path $prepPath -Force | Out-Null
     }
 
-# Major Section: Deploy VDOT Optimizations
+# Major Section: Deploy VDOT Optimizations 
+    # IMPORTANT: This script references scripts and config files in a different gitHub Repository: https://github.com/tvanroo/oger-vdot 
     # -----------------------------------------------------
-    # Define the URL of the ZIP file
-    $zipUrl = "https://github.com/tvanroo/oger-vdot/archive/refs/heads/main.zip"
-
-    # Define the local path for the downloaded ZIP file using the $prepPath variable
-    $zipFilePath = Join-Path -Path $prepPath -ChildPath "downloaded.zip"
-
-    # Download the ZIP file
-    Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
-
-    # Extract the ZIP file to $prepPath
-    Expand-Archive -LiteralPath $zipFilePath -DestinationPath $prepPath -Force
-
-    # Optionally, remove the ZIP file after extraction if not needed
-    Remove-Item -Path $zipFilePath
-    
-    # Construct the full path to the Windows_VDOT.ps1 script using $prepPath
-    $scriptPath = Join-Path -Path $prepPath -ChildPath "oger-vdot-main\Windows_VDOT.ps1"
-
-    # Execute the script with arguments
-    & $scriptPath -Optimizations AppxPackages, Autologgers, DefaultUserSettings, DiskCleanup, NetworkOptimizations, ScheduledTasks, Services -AdvancedOptimizations Edge, RemoveOneDrive -AcceptEULA
-
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tvanroo/oger/main/scripts/Run%20Cutom%20VDOT/run-custom-vdot%202024-04-01.ps1" -OutFile "$prepPath\run-custom-vdot.ps1"; & powershell.exe -NoProfile -ExecutionPolicy Bypass -File "$prepPath\run-custom-vdot.ps1"
 
 # Major Section: Set Timezone to Eastern
     # -----------------------------------------------------
