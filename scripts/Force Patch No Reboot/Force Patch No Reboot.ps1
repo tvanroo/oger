a697878ec913ad1e9e3232070f355a6f1c30c35a
Toby vanRoojen <toby@vanroojen>
2024-06-26 12:38:05 -0700

    Update Force Patch No Reboot.ps1

--- scripts/Force Patch No Reboot/Force Patch No Reboot.ps1
+++ scripts/Force Patch No Reboot/Force Patch No Reboot.ps1
@@ -1,4 +1,33 @@
+# Ensure the Temp directory exists
+if (-not (Test-Path -Path 'C:\Temp')) {
+    New-Item -ItemType Directory -Path 'C:\Temp'
+}
+
+# Generate a unique timestamp for the log file
+$timestamp = Get-Date -Format "yyyyMMddHHmmss"
+$logFilePath = "C:\Temp\updates$timestamp.txt"
+
+# Redirect output to log file
+Start-Transcript -Path $logFilePath
+
+# Install necessary package provider and modules
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module -Name PSWindowsUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Verbose
+
+# Install Windows updates
+$updateResults = Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Verbose | Out-String
+
+# Stop transcript to end logging
+Stop-Transcript
+
+# Determine success or failure
+$logContent = Get-Content -Path $logFilePath | Out-String
+if ($logContent -match "Found \[0\] Updates in pre search criteria" -or $logContent -match "Installation Results: Success") {
+    "SUCCESS" | Out-File -FilePath $logFilePath -Append
+} else {
+    "FAILURE" | Out-File -FilePath $logFilePath -Append
+}
+
+# Return log file path for reference
+$logFilePath
